3523c898cb773bfb26cbc97e9c827052c6ed0998
Ze Huang <huang.ze@linux.dev>
2025-10-22 15:17:32 +0800

    nafnet: switch back to origin nafnet

--- basicsr/archs/naf_mamba_arch.py
+++ basicsr/archs/naf_mamba_arch.py
@@ -123,21 +123,16 @@ class NAFMamba(nn.Module):
         self.decoders_detail = nn.ModuleList()
         self.ups_detail = nn.ModuleList()
 
-        # ... (other decoder initializations remain the same) ...
-
         self.downs = nn.ModuleList()
         self.middle_blks = nn.ModuleList()
 
-        # ========================================================== #
-        # ============== MODIFICATION IS HERE ====================== #
-        # ========================================================== #
         chan = width
         current_size = image_size
         for num in enc_blk_nums:
             # Replace NAFBlock with MPMABlock for the encoder
             self.encoders.append(
                 nn.Sequential(
-                    *[MPMABlock(in_channels=chan, image_size=current_size) for _ in range(num)]
+                    *[NAFBlock(chan) for _ in range(num)]
                 )
             )
             self.downs.append(
@@ -145,8 +140,6 @@ class NAFMamba(nn.Module):
             )
             chan = chan * 2
             current_size //= 2  # Update size for the next encoder level
-        # ========================================================== #
-        # ========================================================== #
 
         for num in middle_blk_num:
             self.middle_blks.append(nn.Sequential(*[NAFBlock(chan) for _ in range(num)]))
