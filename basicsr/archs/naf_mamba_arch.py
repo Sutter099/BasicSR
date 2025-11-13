9912bc3ab8662082b8c15ea697fc56f543c576d0
Ze Huang <huang.ze@linux.dev>
2025-11-13 16:01:03 +0800

    mmnet: mpma in bottleneck, eblock in decoder

--- basicsr/archs/naf_mamba_arch.py
+++ basicsr/archs/naf_mamba_arch.py
@@ -133,7 +133,7 @@ class NAFMamba(nn.Module):
             # Replace NAFBlock with MPMABlock for the encoder
             self.encoders.append(
                 nn.Sequential(
-                    *[EBlock(chan) for _ in range(num)]
+                    *[NAFBlock(chan) for _ in range(num)]
                 )
             )
             self.downs.append(
@@ -143,7 +143,7 @@ class NAFMamba(nn.Module):
             current_size //= 2  # Update size for the next encoder level
 
         for num in middle_blk_num:
-            self.middle_blks.append(nn.Sequential(*[NAFBlock(chan) for _ in range(num)]))
+            self.middle_blks.append(MPMABlock(in_channels=chan, image_size=current_size, depth=num))
 
         # Build the parallel decoders (this part remains unchanged)
         for num in dec_blk_nums:
@@ -157,7 +157,7 @@ class NAFMamba(nn.Module):
 
             chan = chan // 2
 
-            self.decoders_detail.append(nn.Sequential(*[NAFBlock(chan) for _ in range(num)]))
+            self.decoders_detail.append(nn.Sequential(*[EBlock(chan) for _ in range(num)]))
             # self.decoders_lf.append(nn.Sequential(*[NAFBlock(chan, FFN_Expand=1) for _ in range(num // 2 + 1)]))
             # self.decoders_stripe.append(WaveletDenoiseBlock(chan))
 
